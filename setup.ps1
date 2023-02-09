7ac5a050537c0fce7e28804df6a8caad06e1efc5
Bassman2 <ralf.beckers@hotmail.com>
2023-02-10 00:38:53 +0100

    update install

--- setup.ps1
+++ setup.ps1
@@ -20,6 +20,6 @@ $setupDotNet40 = Join-Path -Path $PSScriptRoot -ChildPath "setup\dotNetFx40_Full
 					
 Write-Host "Install .NET Framework 4.0"
 
-. $setupDotNet40 /q /norestart
+cmd /c start /wait $setupDotNet40 /lcid 1031 /passive /norestart
 
 Write-Host ".NET Framework 4.0 installed" -ForegroundColor Green	
